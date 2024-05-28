3fe296f091e09a6a0ffd56bc69ec6df1bec16bf6
thiagochristen <128715585+thiagochristen@users.noreply.github.com>
2024-05-28 08:33:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+cost caixaPrincipal=document.querySelector(".caixa-principal")
+cost caixaPerguntas=document.querySelector(".caixa-perguntas")
+cost caixaAlternativas=document.querySelector(".caixa-alternativas")
+cost caixaResultado=document.querySelector(".caixa-resultado")
+cost textoResultado=document.querySelector(".texto-resultado")
 
+cost perguntas=[
+  {
+     enunciado:"Pergunta 1",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },
